91fdbd81b52c9d509c02dfd94cc9435f380cde3f
k-y-le <41943646+k-y-le@users.noreply.github.com>
2023-01-02 16:15:35 -0800

    mobile version of portal site now live :D

--- js/portal.js
+++ js/portal.js
@@ -25,12 +25,16 @@ function getPortals(){
 }
 function updateLists() {
     $("#past-list").html('');
+    $("#past-list-mobile").html('');
     past.slice().reverse().forEach((element) => {
         $("#past-list").html($("#past-list").html() + '<li>' + element + '</li>');
+        $("#past-list-mobile").html($("#past-list-mobile").html() + '<li>' + element + '</li>');
     });
     $("#future-list").html('');
+    $("#future-list-mobile").html('');
     future.slice().reverse().forEach((element) => {
         $("#future-list").html($("#future-list").html() + '<li>' + element + '</li>');
+        $("#future-list-mobile").html($("#future-list-mobile").html() + '<li>' + element + '</li>');
     });
 }
 var current = 1;
@@ -42,8 +46,64 @@ function nextPortal() {
     //     });
     //    });
     $('#top-img').attr('src', 'img/portal/portal' + current + '.png');
+    $('#img-portal-mobile').attr('src', 'img/portal/portal' + current + '.png');
     current = (current < 20 - 1) ? current + 1 : 1;
 }
+
+$("#portal-past-mobile").click(function() {
+    if ($("#mobile-col-past").is(":visible")) {
+        // only show main page in middle col
+        $("#mobile-col-main").show();
+        $("#mobile-col-future").hide();
+        $("#mobile-col-past").hide();
+        // past word no longer underlined and back to inactive portal image
+        $("#past-link").css("text-decoration", "none");
+        $('#past-portal-mobile').attr('src', 'img/portal/portal-past-mobile.png');
+        // change background color to black
+        $("body").css('background-color', '#16161E');
+        // show years on main page
+        $(".fixed-year").show();
+    } else {
+        // only show past items in portal
+        $("#mobile-col-main").hide();
+        $("#mobile-col-future").hide();
+        $("#mobile-col-past").show();
+        // underline past word and add new portal image
+        $("#past-link").css("text-decoration", "underline");
+        $('#past-portal-mobile').attr('src', 'img/portal/portal-past-mobile-active.png');
+        // change background color to purple
+        $("body").css('background-color', '#561F55');
+        // hide years from main page
+        $(".fixed-year").hide();
+    }
+})
+$("#portal-future-mobile").click(function() {
+    if ($("#mobile-col-future").is(":visible")) {
+        // only show main page in middle col
+        $("#mobile-col-main").show();
+        $("#mobile-col-past").hide();
+        $("#mobile-col-future").hide();
+        // future word no longer underlined and back to inactive portal image
+        $("#future-link").css("text-decoration", "none");
+        $('#future-portal-mobile').attr('src', 'img/portal/portal-future-mobile.png');
+        // change background color to black
+        $("body").css('background-color', '#16161E');
+        // show years on main page
+        $(".fixed-year").show();
+    } else {
+        // only show past items in portal
+        $("#mobile-col-main").hide();
+        $("#mobile-col-past").hide();
+        $("#mobile-col-future").show();
+        // underline past word and add new portal image
+        $("#future-link").css("text-decoration", "underline");
+        $('#future-portal-mobile').attr('src', 'img/portal/portal-future-mobile-active.png');
+        // change background color to green
+        $("body").css('background-color', '#293415');
+        // hide years from main page
+        $(".fixed-year").hide();
+    }
+})
 
 getPortals();
 
