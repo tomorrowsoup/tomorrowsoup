8712ef68ab6f3749e11b2a108216951755f07c5d
k-y-le <41943646+k-y-le@users.noreply.github.com>
2023-01-05 15:48:06 -0800

    add fade in and out of text itself, and a smooth animation of fading in and out of the portal image on the stairs -- also mobile compatible

--- js/portal.js
+++ js/portal.js
@@ -36,18 +36,40 @@ function updateLists() {
         $("#future-list").html($("#future-list").html() + '<li>' + element + '</li>');
         $("#future-list-mobile").html($("#future-list-mobile").html() + '<li>' + element + '</li>');
     });
+    // ambient twinkling look
+    $("#past-list > li").each(function () {
+        $(this).css("animation", "blinker " + (Math.random() * (10 - 3) + 3) + "s infinite")
+    });
+    $("#future-list > li").each(function () {
+        $(this).css("animation", "blinker " + (Math.random() * (10 - 3) + 3) + "s infinite")
+    });
+    $("#past-list-mobile > li").each(function () {
+        $(this).css("animation", "blinker " + (Math.random() * (10 - 3) + 3) + "s infinite")
+    });
+    $("#future-list-mobile > li").each(function () {
+        $(this).css("animation", "blinker " + (Math.random() * (10 - 3) + 3) + "s infinite")
+    });
 }
 var current = 1;
+var secondPortal = true;
 function nextPortal() {
-    // $("#img-portal").fadeTo(100, 0.8, function() {
-    //     $('#img-portal').attr('src', 'img/portal/portal' + current + '.png');
-    //     $("#img-portal").on('load', function(){
-    //       $("#img-portal").fadeTo(50,1);
-    //     });
-    //    });
-    $('#top-img').attr('src', 'img/portal/portal' + current + '.png');
+    // fade in images consecutively
+    if (secondPortal) {
+        $('#top-img-2').attr('src', 'img/portal/portal' + current + '.png');
+        $('#img-portal-mobile-2').attr('src', 'img/portal/portal' + current + '.png');
+        $('#img-portal-mobile-1').fadeOut(2000);
+        $('#top-img-1').fadeOut(2000);
+        secondPortal = false;
+        current = (current < 20 - 1) ? current + 1 : 1;
+    } else {
+        $('#top-img-1').attr('src', 'img/portal/portal' + current + '.png');
+        $('#img-portal-mobile-1').attr('src', 'img/portal/portal' + current + '.png');
+        $('#img-portal-mobile-1').fadeIn(2000);
+        $('#top-img-1').fadeIn(2000);
+        secondPortal = true;
+        current = (current < 20 - 1) ? current + 1 : 1;
+    }
     $('#img-portal-mobile').attr('src', 'img/portal/portal' + current + '.png');
-    current = (current < 20 - 1) ? current + 1 : 1;
 }
 
 $("#portal-past-mobile").click(function() {
@@ -56,10 +78,12 @@ $("#portal-past-mobile").click(function() {
         $("#mobile-col-main").show();
         $("#mobile-col-future").hide();
         $("#mobile-col-past").hide();
-        // past word no longer underlined and back to inactive portal image
+        // past word no longer underlined and back to inactive portal image + main portal animation appears
         $("#past-link").css("text-decoration", "none");
         $("#future-link").css("text-decoration", "none");
         $('#past-portal-mobile').attr('src', 'img/portal/portal-past-mobile.png');
+        $('#img-portal-mobile-1').removeClass("hide");
+        $('#img-portal-mobile-2').removeClass("hide");
         // change background color to black
         $("body").css('background-color', '#16161E');
         // show years on main page
@@ -69,10 +93,12 @@ $("#portal-past-mobile").click(function() {
         $("#mobile-col-main").hide();
         $("#mobile-col-future").hide();
         $("#mobile-col-past").show();
-        // underline past word and add new portal image
+        // underline past word and add new portal image + remove main portal animation
         $("#past-link").css("text-decoration", "underline");
         $("#future-link").css("text-decoration", "none");
         $('#past-portal-mobile').attr('src', 'img/portal/portal-past-mobile-active.png');
+        $('#img-portal-mobile-1').addClass("hide");
+        $('#img-portal-mobile-2').addClass("hide");
         // change background color to purple
         $("body").css('background-color', '#561F55');
         // hide years from main page
@@ -85,10 +111,12 @@ $("#portal-future-mobile").click(function() {
         $("#mobile-col-main").show();
         $("#mobile-col-past").hide();
         $("#mobile-col-future").hide();
-        // future/past word no longer underlined and back to inactive portal image
+        // future/past word no longer underlined and back to inactive portal image + main portal animation appears
         $("#future-link").css("text-decoration", "none");
         $("#past-link").css("text-decoration", "none");
         $('#future-portal-mobile').attr('src', 'img/portal/portal-future-mobile.png');
+        $('#img-portal-mobile-1').removeClass("hide");
+        $('#img-portal-mobile-2').removeClass("hide");
         // change background color to black
         $("body").css('background-color', '#16161E');
         // show years on main page
@@ -98,10 +126,12 @@ $("#portal-future-mobile").click(function() {
         $("#mobile-col-main").hide();
         $("#mobile-col-past").hide();
         $("#mobile-col-future").show();
-        // underline future word and add new portal image
+        // underline future word and add new portal image + remove main portal animation
         $("#future-link").css("text-decoration", "underline");
         $("#past-link").css("text-decoration", "none");
         $('#future-portal-mobile').attr('src', 'img/portal/portal-future-mobile-active.png');
+        $('#img-portal-mobile-1').addClass("hide");
+        $('#img-portal-mobile-2').addClass("hide");
         // change background color to green
         $("body").css('background-color', '#293415');
         // hide years from main page
@@ -115,4 +145,4 @@ setInterval(function(){
     getPortals();
 }, 30000)
 
-setInterval(nextPortal, 2000);
+setInterval(nextPortal, 4000);
